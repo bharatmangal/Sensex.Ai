3a3744cbcf26bcba0880fe53ba1556c70e26be0b
Bhaaaarat <116515728+Bhaaaarat@users.noreply.github.com>
2023-09-11 05:53:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import pandas_datareader as data
 from keras.models import load_model
 import yfinance as yf
 import streamlit as st
